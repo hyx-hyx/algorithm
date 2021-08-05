575bbd3f9c86d97dc761da1e58524e525740f611
hyx-hyx <315024424@qq.com>
2021-08-05 23:17:13 +0800

    new

--- 模板/欧拉筛.cpp
+++ 模板/欧拉筛.cpp
@@ -1,8 +1,9 @@
+#define maxn 
 int prime[maxn];
 int visit[maxn];
 void Prime(){
-    mem(visit,0);
-    mem(prime, 0);
+    memset(visit,0,sizeof(prime));
+    memset(prime, 0,sizeof(prime));
     for (int i = 2;i <= maxn; i++) {
         if (!visit[i]) {
             prime[++prime[0]] = i;      
