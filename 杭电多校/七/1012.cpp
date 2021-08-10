1eecdae1b414319d01f30572a1242044b6455d84
hyx-hyx <315024424@qq.com>
2021-08-11 00:15:11 +0800

    new

--- 杭电多校/七/1012.cpp
+++ 杭电多校/七/1012.cpp
@@ -8,53 +8,7 @@
 #include<queue>
 #include<algorithm>
 using namespace std;
-#define maxn 5000005
-#define mod 998244353
-typedef long long ll;
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	string s;
-	int t;
-	cin >> t;
-	int dp[maxn][maxn][27];
-	while (t--)
-	{
-		cin >> s;
-		int maxchar = 0;
-		for (int i = 0; i < s.size(); ++i)
-			maxchar = max((int)s[i],maxchar);
-		for (int i = 1; i <= s.size(); ++i)
-			dp[0][i][s[0]] = 0;
-		for (int i = 1; i <= s.size(); ++i)
-		{
-			for (int j = i; j <= s.size(); ++j)
-			{
-				if ()
-					dp[i][j][c] = ( dp[i - 1][j][c] - 1 ) * ( dp[i - 1][j][c] - 1 ) % mod;
-				else
-					dp[i][j][c] = dp[i - 1][j][c] * dp[i - 1][j][c] % mod;
-			}
-		}
-		for(int i=1;i<=s.size();++i)
-		cout<< <<endl;
-	}
-	return 0;
-}
-//===========================================================
-#define _CRT_SECURE_NO_WARNINGS
-#include<cstdio>
-#include<iostream>
-#include<vector>
-#include<cstring>
-#include<assert.h>
-#include<string>
-#include<queue>
-#include<algorithm>
-using namespace std;
-#define maxn 5005
+#define maxn 100005
 #define mod 998244353
 typedef long long ll;
 int main()
@@ -64,35 +18,57 @@ int main()
 	getchar();
 	while (t--)
 	{
-		int sum[maxn] = { 0 };
-		int flag[27] = { 0 };
-		char c = 0;
-		char ch[maxn];
+		char temp = 0;
+		char charr[maxn];
+		vector<char> ch;
 		int cnt = 0;
-		int kind = 0;
-		while (c != '\n')
+		int flag[27] = { 0 };
+		while (1)
 		{
-			scanf("%c", &c);
-			if (!flag[c - 97])
-				kind++;
-			flag[c - 97]++;
-			ch[cnt++] = c;
+			scanf("%c", &temp);
+			if (temp == '\n') break;
+			charr[cnt++] = temp;
+			if (!flag[temp - 97])
+				flag[temp - 97] = 1, ch.push_back(temp);
 		}
-
-		for (int i = 0; i < cnt - 1; ++i)
+		charr[cnt] = 0;
+		string s(charr);
+		int n = s.size();
+		vector<vector<vector<int> > >dp(2, vector<vector<int> >(n, vector<int>(27, 0)));
+		int sum = 0;
+		dp[0][0][s[0] - 97]++;
+		sum = 1;
+		for (int i = 1; i < s.size(); ++i)
 		{
-			int temp = 0;
-			if (i)
-				flag[ch[i-1] - 97]--;
-			for (int j = 0; j < 27; ++j)
-				temp = ( temp + ( flag[j] * flag[j] % mod ) ) % mod;
-			sum[i] = temp;
+			dp[0][i][s[i] - 97] = dp[0][i - 1][s[i] - 97] + 1;
+			sum = ( sum + ( dp[0][i][s[i] - 97] * dp[0][i][s[i] - 97] ) % mod ) % mod;
+			for (int j = 0; j < ch.size(); ++j)
+			{
+				int c = ch[j] - 97;
+				if (s[i] - 97 == c)
+					continue;
+				else
+					dp[0][i][c] = dp[0][i - 1][c], sum = ( sum + ( dp[0][i][c] * dp[0][i][c] ) % mod ) % mod;
+			}
 		}
-		int ans = 0;
-		int temp = cnt;
-		for (int i = 1; i < cnt; ++i)
-			ans = ( ans + ( sum[i - 1] * i) % mod ) % mod;
-		cout << ans << endl;
+		for (int i = 1; i < s.size(); ++i)
+		{
+			int index = i & 1;
+			for (int j = i; j < s.size(); ++j)
+			{
+				for (int c = 0; c < ch.size(); ++c)
+				{
+					int k = ch[c] - 97;
+					int temp = 0;
+					if (s[i - 1] == k + 97)
+						dp[index][j][k] = dp[index ^ 1][j][k] - 1;
+					else
+						dp[index][j][k] = dp[index ^ 1][j][k];
+					sum = ( sum + ( dp[index][j][k] * dp[index][j][k] ) % mod ) % mod;
+				}
+			}
+		}
+		printf("%d\n", sum);
 	}
 	return 0;
 }
