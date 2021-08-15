0c407630b334022dc56201a06a74c6c6b6fada7d
hyx-hyx <315024424@qq.com>
2021-08-15 11:19:27 +0800

    new

--- 模板/可持久化线段树.cpp
+++ 模板/可持久化线段树.cpp
@@ -1,10 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+typedef long long ll;
+using namespace std;
+const int MAXV = 20000000, MAXN = 1000005;
+#define ls(x) tree[x].ls
+#define rs(x) tree[x].rs
+#define val(x) tree[x].val
+#define mark(x) tree[x].mark
+struct node
+{
+    int val, ls, rs;
+    int mark;//涉及区间修改时加上，否则可删去
+} tree[MAXV];
+int A[MAXN], roots[MAXN], n, m, cnt = 1; // roots记录每个历史版本的根节点
 void build(int l = 1, int r = n, int p = 1)
 {
     if (l == r)
         val(p) = A[l];
     else
     {
-        ls(p) = ++cnt, rs(p) = ++cnt; // 新建节点
+        ls(p) = ++cnt, rs(p) = ++cnt;
         int mid = (l + r) / 2;
         build(l, mid, ls(p));
         build(mid + 1, r, rs(p));
@@ -12,7 +30,24 @@ void build(int l = 1, int r = n, int p = 1)
     }
 }
 // 令A_x加d，p表示原版本的节点，q表示新版本的节点
-void update(int l, int r, int d, int p, int q, int cl = 1, int cr = n) // 区间修改
+//单点修改
+void update_point(int x, int d, int p, int q, int cl = 1, int cr = n)
+{
+    if (cl == cr)
+        val(q) = val(p) + d; // 给叶子节点赋值
+    else
+    {
+        ls(q) = ls(p), rs(q) = rs(p); // 复制节点
+        int mid = (cl + cr) / 2;
+        if (x <= mid)
+            ls(q) = ++cnt, update_point(x, d, ls(p), ls(q), cl, mid); // 创建新节点作为左儿子，然后往左递归
+        else
+            rs(q) = ++cnt, update_point(x, d, rs(p), rs(q), mid + 1, cr); // 创建新节点作为右儿子，然后往右递归
+        val(q) = val(ls(q)) + val(rs(q)); // 根据子节点给当前节点赋值
+    }
+}
+// 令A_x加d，p表示原版本的节点，q表示新版本的节点
+void update_range(int l, int r, int d, int p, int q, int cl = 1, int cr = n) // 区间修改
 {
     ls(q) = ls(p), rs(q) = rs(p), mark(q) = mark(p); // 复制节点
     if (cl >= l && cr <= r)
@@ -24,13 +59,25 @@ void update(int l, int r, int d, int p, int q, int cl = 1, int cr = n) // 区间
     {
         int mid = (cl + cr) / 2;
         if (cl <= r && mid >= l) // 提前进行判断，以免新建不必要的节点
-            ls(q) = ++cnt, update(l, r, d, ls(p), ls(q), cl, mid);
+            ls(q) = ++cnt, update_range(l, r, d, ls(p), ls(q), cl, mid);
         if (mid + 1 <= r && cr >= l)
-            rs(q) = ++cnt, update(l, r, d, rs(p), rs(q), mid + 1, cr);
+            rs(q) = ++cnt, update_range(l, r, d, rs(p), rs(q), mid + 1, cr);
     }
     val(q) = val(p) + (min(cr, r) - max(cl, l) + 1) * d; // 根据需要更新的区间长度计算当前节点的值
 }
-ll query(int l, int r, int p, int cl = 1, int cr = n, ll mk = 0) // 区间查询
+int query_nomark(int l, int r, int p, int cl = 1, int cr = n) // 不带标记区间查询
+{
+    if (cl > r || cr < l)
+        return 0;
+    else if (cl >= l && cr <= r)
+        return val(p);
+    else
+    {
+        int mid = (cl + cr) / 2;
+        return query_nomark(l, r, ls(p), cl, mid) + query_nomark(l, r, rs(p), mid + 1, cr);
+    }
+}
+ll query_mark(int l, int r, int p, int cl = 1, int cr = n, ll mk = 0) // 区间查询
 {
     if (cl > r || cr < l)
         return 0;
@@ -39,6 +86,6 @@ ll query(int l, int r, int p, int cl = 1, int cr = n, ll mk = 0) // 区间查询
     else
     {
         int mid = (cl + cr) / 2;
-        return query(l, r, ls(p), cl, mid, mk + mark(p)) + query(l, r, rs(p), mid + 1, cr, mk + mark(p)); // 带着标记传递
+        return query_mark(l, r, ls(p), cl, mid, mk + mark(p)) + query_mark(l, r, rs(p), mid + 1, cr, mk + mark(p)); // 带着标记传递
     }
 }
