cfb176ed0ab83e8ede86f4de8b154f6f404e1559
hyx-hyx <315024424@qq.com>
2021-08-20 00:02:50 +0800

    new

--- 杭电多校/十/1003.cpp
+++ 杭电多校/十/1003.cpp
@@ -1,45 +1,33 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<iostream>
 #include<cstdio>
-#include<cstring>
+#include<iostream>
 #include<vector>
+#include<cstring>
+#include<assert.h>
+#include<string>
+#include<queue>
+#include<algorithm>
+#include<bitset>
 using namespace std;
-vector<vector<int> > dp;
-void slove(int n)
-{
-    int m = 0;
-    dp.assign(n+1,vector<int>((n*(n-1)/2+10),0));
-    dp[0][0] = 1; dp[1][0] = 1;
-    for(int i = 2; i<=n; i++)
-    {
-        for(int k = 0; k<i; k++)
-        {
-            for(int j = 0; j<=k*( k-1 )/2; j++)
-            {
-                if(dp[k][j])
-                {
-                    m = j+( i-k )*k;
-                    dp[i][m] = 1;
-                }
-            }
-        }
-    }
-}
+typedef long long ll;
+const int maxn = 31500;
 int main()
 {
-    int t;
-    cin>>t;
-    while(t--)
-    {    
-        int n;
-        cin>>n;
-        slove(n);
-        for(int i = 0; i<=n*( n-1 )/2; i++)
-        {
-            if(dp[n][i])
-                printf(i==0 ? "%d" : " %d",i);
-        }
-        printf("\n");
-    }
-    return 0;
+	bitset<maxn> dp[710];
+	int t;
+	cin >> t;
+	dp[0][0] = 1;
+	for(int i = 1; i < 710; ++i)
+		for(int j = 1; j <= i; ++j)
+			dp[i] |= dp[i - j] << (j * (i - j));
+	while(t--)
+	{
+		int n;
+		cin >> n;
+		int lim = min(maxn-1,n * (n - 1) / 2);
+		for(int i = 0; i < lim; i++) if(dp[n][i]) printf("%d ",i);	
+		for(int i = lim + 1; i < n * (n - 1)/2; i++) printf("%d ",i);
+		putchar('\n');
+	}
+	return 0;
 }
