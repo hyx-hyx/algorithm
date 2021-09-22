102685164aa18fe58ec3123645c976603e854003
hyx-hyx <315024424@qq.com>
2021-09-22 23:41:57 +0800

    new

--- 模板/SPFA.cpp
+++ 模板/SPFA.cpp
@@ -1,32 +1,3 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include <queue>
-#include <cmath>
-#define pointcount 100
-#define edgescount 100
-#define inf 1e9
-using namespace std;
-struct edge
-{
-    int to;
-    int w;
-    int next;
-};
-typedef pair<int, int> Pair;
-priority_queue<Pair, vector<Pair>, greater<Pair> > Q_dij;
-vector<edge> edges(edgescount);
-int cnt = 0;
-vector<int> head(pointcount, 0);
-vector<int> dist(pointcount, inf);
-void add(int from,int to,int w)
-{
-    edges[++cnt].to=to;
-    edges[cnt].w = w;
-    edges[cnt].next = head[from];
-    head[from] = cnt;
-}
 void SPFA(int s)
 {
     vector<int> inqueue(pointcount, 0);
@@ -54,27 +25,6 @@ void SPFA(int s)
         }
     }
 }
-void dij(int s)
-{
-    vector<int> vis(pointcount, 0);
-    dist[s]=0;
-    Q_dij.push( make_pair(0, s));
-    while(!Q_dij.empty())
-    {
-        int p=Q_dij.top().second;
-        Q_dij.pop();
-        if(vis[p])
-            continue;
-        vis[p] = 1;
-        for (int e = head[p]; e != 0;e=edges[e].next)
-        {
-            int to = edges[e].to;
-            dist[to] = min(dist[to], dist[p] + edges[e].w);
-            if(!vis[to])
-                Q_dij.push( make_pair(dist[to],to));
-        }
-    }
-} 
 int main()
 {
     int n, m, s;
